ce00abe291c02fdaf289f25e4f26d59c747d1fa0
Hugo KAHLOUCH <hugo.kahlouch@hotmail.fr>
2020-12-05 12:24:46 +0100

    problem 5

--- src/main/java/fr/kahlouch/advent/problem2020/Problem05.java
+++ src/main/java/fr/kahlouch/advent/problem2020/Problem05.java
@@ -2,8 +2,83 @@ package fr.kahlouch.advent.problem2020;
 
 import fr.kahlouch.advent.ProblemResolver;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
 public class Problem05 {
     public static void main(String[] args) {
-        new ProblemResolver("problem2020/problem05.txt").resolve();
+        new ProblemResolver("problem2020/problem05.txt", Problem05::rule1, Problem05::rule2).resolve();
+    }
+
+    private static int[] initArray(int size) {
+        int[] array = new int[size];
+        for (int i = 0; i < size; ++i) {
+            array[i] = i;
+        }
+        return array;
+    }
+
+    private static int[] backOrRight(int[] rowOrColumn) {
+        return Arrays.copyOfRange(rowOrColumn, rowOrColumn.length / 2, rowOrColumn.length);
+    }
+
+    private static int[] frontOrLeft(int[] rowOrColumn) {
+        return Arrays.copyOfRange(rowOrColumn, 0, rowOrColumn.length / 2);
+    }
+
+    private static Map<Character, Function<int[], int[]>> getActionMap() {
+        return Map.ofEntries(
+                Map.entry('F', Problem05::frontOrLeft),
+                Map.entry('L', Problem05::frontOrLeft),
+                Map.entry('B', Problem05::backOrRight),
+                Map.entry('R', Problem05::backOrRight)
+        );
+    }
+
+    public static String rule1(List<String> input) {
+        Map<Character, Function<int[], int[]>> actionMap = getActionMap();
+        int max = 0;
+        for (String line : input) {
+            int[] rows = initArray(128);
+            int[] columns = initArray(8);
+            for (int i = 0; i < 7; ++i) {
+                rows = actionMap.get(line.charAt(i)).apply(rows);
+            }
+            for (int i = 7; i < 10; ++i) {
+                columns = actionMap.get(line.charAt(i)).apply(columns);
+            }
+            int result = rows[0] * 8 + columns[0];
+            if (max < result) {
+                max = result;
+            }
+        }
+        return max + "";
+    }
+
+    public static String rule2(List<String> input) {
+        Map<Character, Function<int[], int[]>> actionMap = getActionMap();
+        List<Integer> ids = new ArrayList<>();
+        for (String line : input) {
+            int[] rows = initArray(128);
+            int[] columns = initArray(8);
+            for (int i = 0; i < 7; ++i) {
+                rows = actionMap.get(line.charAt(i)).apply(rows);
+            }
+            for (int i = 7; i < 10; ++i) {
+                columns = actionMap.get(line.charAt(i)).apply(columns);
+            }
+            ids.add(rows[0] * 8 + columns[0]);
+        }
+
+        ids.sort(Integer::compareTo);
+        for(int i=0; i<ids.size()-1; ++i) {
+            if(ids.get(i+1) != ids.get(i) + 1) {
+                return (ids.get(i) + 1) + "";
+            }
+        }
+        return "NOTHING";
     }
 }
