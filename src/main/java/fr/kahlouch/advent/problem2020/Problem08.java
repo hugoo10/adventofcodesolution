8cd22f61f5ffcf3fa6009d54e270aa4c38bf0092
ehka4080 <hugo.kahlouch-ext@pole-emploi.fr>
2020-12-08 11:29:50 +0100

    Problem08

--- src/main/java/fr/kahlouch/advent/problem2020/Problem08.java
+++ src/main/java/fr/kahlouch/advent/problem2020/Problem08.java
@@ -2,8 +2,148 @@ package fr.kahlouch.advent.problem2020;
 
 import fr.kahlouch.advent.ProblemResolver;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class Problem08 {
     public static void main(String[] args) {
-        new ProblemResolver("problem2020/problem08.txt").resolve();
+        new ProblemResolver("problem2020/problem08.txt", Problem08::rule1, Problem08::rule2).resolve();
+    }
+
+    private static String rule1(List<String> input) {
+        List<Command> commands = getCommands(input, -1);
+        List<Command> historic = new ArrayList<>();
+        int idx = 0;
+        int value = 0;
+
+        boolean ok;
+
+        do {
+            ok = true;
+            Command toExecute = commands.get(idx);
+            Out out = toExecute.execute();
+            value += out.deltaValue;
+            idx += out.deltaIdx;
+            final int toTestIdx = idx;
+            if (historic.stream().anyMatch(c -> c.idx == toTestIdx)) {
+                ok = false;
+            }
+            historic.add(toExecute);
+        } while (ok && idx < input.size());
+
+        return value + "";
+    }
+
+    private static String rule2(List<String> input) {
+        boolean ok;
+        int toSwap = 0;
+        int value;
+        do {
+            System.out.println(toSwap + " " + input.size());
+            List<Command> commands = getCommands(input, toSwap);
+            List<Command> historic = new ArrayList<>();
+            int idx = 0;
+            value = 0;
+            do {
+                ok = true;
+                Command toExecute = commands.get(idx);
+                Out out = toExecute.execute();
+                value += out.deltaValue;
+                idx += out.deltaIdx;
+                final int toTestIdx = idx;
+                if (historic.stream().anyMatch(c -> c.idx == toTestIdx)) {
+                    ok = false;
+                }
+                historic.add(toExecute);
+            } while (ok && idx < input.size());
+            toSwap++;
+
+        } while (!ok && toSwap < input.size());
+        return value + "";
+    }
+
+    public static List<Command> getCommands(List<String> input, int toSwap) {
+        List<Command> commands = new ArrayList<>();
+        for (int i = 0; i < input.size(); ++i) {
+            Command command = new Command(input.get(i), i);
+            if (toSwap == i) {
+                command = command.swap();
+            }
+            commands.add(command);
+        }
+        return commands;
+    }
+
+    static class Command {
+        int idx;
+        Supplier<Out> action;
+        int argument;
+        String actionStr;
+
+        Command(String line, int idx) {
+            this(idx, Integer.parseInt(line.split(" ")[1]), line.split(" ")[0]);
+        }
+
+        public Command(int idx, int argument, String actionStr) {
+            this.idx = idx;
+            this.argument = argument;
+            this.actionStr = actionStr;
+            this.action = switch (this.actionStr) {
+                case "acc" -> this::acc;
+                case "jmp" -> this::jmp;
+                case "nop" -> this::nop;
+                default -> throw new RuntimeException("NO COMMAND FOUND");
+            };
+        }
+
+        @Override
+        public String toString() {
+            return "Command{" +
+                    "idx=" + idx +
+                    ", argument=" + argument +
+                    ", actionStr='" + actionStr + '\'' +
+                    '}';
+        }
+
+        public Command swap() {
+            String newCommand = switch (this.actionStr) {
+                case "jmp" -> "nop";
+                case "nop" -> "jmp";
+                case "acc" -> "acc";
+                default -> throw new RuntimeException("BUG");
+            };
+            return this.setAction(newCommand);
+        }
+
+        private Command setAction(String actionStr) {
+            return new Command(this.idx, this.argument, actionStr);
+        }
+
+        private Out acc() {
+            return new Out(this.argument, 1);
+        }
+
+        private Out jmp() {
+            return new Out(0, this.argument);
+        }
+
+        private Out nop() {
+            return new Out(0, 1);
+        }
+
+        public Out execute() {
+            return this.action.get();
+        }
+    }
+
+    static class Out {
+        int deltaValue;
+        int deltaIdx;
+
+        public Out(int deltaValue, int deltaIdx) {
+            this.deltaValue = deltaValue;
+            this.deltaIdx = deltaIdx;
+        }
     }
 }
