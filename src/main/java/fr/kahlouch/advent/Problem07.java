2965d479d7515980e84d07bcb5c6773d88638e24
hugo <hugo.kahlouch@hotmail.fr>
2021-12-07 08:05:19 +0100

    problem 7

--- src/main/java/fr/kahlouch/advent/Problem07.java
+++ src/main/java/fr/kahlouch/advent/Problem07.java
@@ -1,18 +1,36 @@
 package fr.kahlouch.advent;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Problem07 extends Problem {
+    List<Integer> orderedPositions;
+
     @Override
     public void setupData() {
-
+        orderedPositions = Arrays.stream(this.input.get(0).split(",")).map(Integer::parseInt).sorted().toList();
     }
 
     @Override
     public Object rule1() {
-        return null;
+        var size = this.orderedPositions.size();
+        var mediane = this.orderedPositions.get(size / 2);
+
+        return this.orderedPositions.parallelStream().map(v -> Math.abs(v - mediane)).reduce(0, Integer::sum);
     }
 
     @Override
     public Object rule2() {
-        return null;
+        var sum = this.orderedPositions.parallelStream().reduce(0, Integer::sum);
+        var moyenneUR = sum / (double) this.orderedPositions.size();
+        var moyenne = Math.round(moyenneUR) - 1;
+        return this.orderedPositions.parallelStream().map(v -> {
+            var diff = Math.abs(v - moyenne);
+            var s = 0L;
+            for (var i = 0; i < diff; ++i) {
+                s += (i + 1);
+            }
+            return s;
+        }).reduce(0L, Long::sum);
     }
 }
--- src/main/java/fr/kahlouch/advent/_Solver.java
+++ src/main/java/fr/kahlouch/advent/_Solver.java
@@ -31,7 +31,7 @@ public class _Solver {
     );
 
     public static void main(String[] args) {
-        int toSolve = 6;
+        int toSolve = 7;
         ProblemSolver.solve(String.format("problem%02d.txt", toSolve), problems.get(toSolve - 1));
     }
 
