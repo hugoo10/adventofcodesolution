4af2a3ccaa65d242b864a51a841efbd27d273052
Hugo KAHLOUCH <hugo.kahlouch@hotmail.fr>
2022-12-04 07:52:37 +0100

    problem 3

--- src/main/java/fr/kahlouch/advent/_Solver.java
+++ src/main/java/fr/kahlouch/advent/_Solver.java
@@ -33,7 +33,7 @@ public class _Solver {
     );
 
     public static void main(String[] args) {
-        int toSolve = 2;
+        int toSolve = 3;
         ProblemSolver.solve(String.format("problem%02d.txt", toSolve), problems.get(toSolve - 1));
     }
 
