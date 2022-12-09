f55c981ded59dfffb61d65d4e3b75347c970df17
Hugo KAHLOUCH <hugo.kahlouch@hotmail.fr>
2022-12-09 09:11:10 +0100

    problem 9 -unsolved

--- src/main/java/fr/kahlouch/advent/_Solver.java
+++ src/main/java/fr/kahlouch/advent/_Solver.java
@@ -33,7 +33,7 @@ public class _Solver {
     );
 
     public static void main(String[] args) {
-        int toSolve = 8;
+        int toSolve = 9;
         ProblemSolver.solve(String.format("problem%02d.txt", toSolve), problems.get(toSolve - 1));
     }
 
