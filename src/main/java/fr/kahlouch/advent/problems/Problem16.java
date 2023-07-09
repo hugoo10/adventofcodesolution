38c6e04a676dcbf4f8c724e6c17cb27bb7adb449
Hugo KAHLOUCH <hugo.kahlouch@hotmail.fr>
2023-07-09 10:01:13 +0200

    problem 16

--- src/main/java/fr/kahlouch/advent/problems/Problem16.java
+++ src/main/java/fr/kahlouch/advent/problems/Problem16.java
@@ -2,10 +2,159 @@ package fr.kahlouch.advent.problems;
 
 import fr.kahlouch.advent.Problem;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Problem16 extends Problem {
+
+    Graph graph;
+
+    record Node(String id, int rate, int released) {
+
+        Node(String id, int rate) {
+            this(id, rate, -1);
+        }
+    }
+
+
+    static class RouteNode implements Comparable<RouteNode> {
+        Node current;
+        Node previous;
+        double routeScore;
+        double estimatedScore;
+        Map<String, Integer> visited;
+
+        public RouteNode(Node current, Node previous, double routeScore, double estimatedScore, Map<String, Integer> visited) {
+            this.current = current;
+            this.previous = previous;
+            this.routeScore = routeScore;
+            this.estimatedScore = estimatedScore;
+            this.visited = new TreeMap<>(visited);
+        }
+
+        RouteNode(Node current, Map<String, Integer> visited) {
+            this(current, null, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, visited);
+        }
+
+        @Override
+        public int compareTo(RouteNode other) {
+            return Double.compare(this.estimatedScore, other.estimatedScore);
+        }
+
+        public Node getCurrent() {
+            return current;
+        }
+
+        public void setCurrent(Node current) {
+            this.current = current;
+        }
+
+        public Node getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Node previous) {
+            this.previous = previous;
+        }
+
+        public double getRouteScore() {
+            return routeScore;
+        }
+
+        public void setRouteScore(double routeScore) {
+            this.routeScore = routeScore;
+        }
+
+        public double getEstimatedScore() {
+            return estimatedScore;
+        }
+
+        public void setEstimatedScore(double estimatedScore) {
+            this.estimatedScore = estimatedScore;
+        }
+    }
+
+    record RouteFinder(Graph graph, Scorer nextNodeScorer, Scorer targetScorer) {
+        public List<Node> findRoute(Node from, Node to) {
+            Queue<RouteNode> openSet = new PriorityQueue<>();
+            Map<Node, RouteNode> allNodes = new HashMap<>();
+
+            RouteNode start = new RouteNode(from, null, 0d, targetScorer.computeCost(from, to), new HashMap<>());
+            openSet.add(start);
+            allNodes.put(from, start);
+
+            while (!openSet.isEmpty()) {
+                RouteNode next = openSet.poll();
+                //build route
+                if (next.getCurrent().equals(to)) {
+                    List<Node> route = new ArrayList<>();
+                    RouteNode current = next;
+                    do {
+                        route.add(0, current.getCurrent());
+                        current = allNodes.get(current.getPrevious());
+                    } while (current != null);
+                    return route;
+                }
+                graph.getConnections(next.getCurrent()).forEach(connection -> {
+                    var visited = new TreeMap<>(next.visited);
+//                    visited.add(connection.id);
+                    RouteNode nextNode = allNodes.getOrDefault(connection, new RouteNode(connection, visited));
+                    allNodes.put(connection, nextNode);
+
+                    double newScore = next.getRouteScore() + nextNodeScorer.computeCost(next.getCurrent(), connection);
+                    if (newScore < nextNode.getRouteScore()) {
+                        nextNode.setPrevious(next.getCurrent());
+                        nextNode.setRouteScore(newScore);
+                        nextNode.setEstimatedScore(newScore + targetScorer.computeCost(connection, to));
+                        openSet.add(nextNode);
+                    }
+                });
+            }
+            throw new IllegalStateException("No route found");
+        }
+    }
+
+    record Graph(Map<String, Node> nodes, Map<String, List<String>> graph) {
+        public Node getNode(String id) {
+            return nodes.get(id);
+        }
+
+        public Set<Node> getConnections(Node node) {
+            return graph.get(node.id).stream()
+                    .map(this::getNode)
+                    .collect(Collectors.toSet());
+        }
+
+    }
+
+    record Scorer() {
+        double computeCost(Node from, Node to) {
+            return 1;
+        }
+    }
+
     @Override
     public void setupData() {
+        Map<String, Node> nodes = new HashMap<>();
+        Map<String, List<String>> graphMap = new HashMap<>();
+        lines.forEach(line -> {
+            var nodeIdLeft = line.replace("Valve ", "").split(" has flow rate=");
+            var nodeId = nodeIdLeft[0];
+            var rateLeft = nodeIdLeft[1].split("; tunnels? leads? to valves? ");
+            var rate = Integer.parseInt(rateLeft[0]);
+            var linkedNode = rateLeft[1].split(", ");
+            var node = new Node(nodeId, rate);
+            nodes.put(node.id, node);
+            graphMap.put(node.id, List.of(linkedNode));
+        });
+        graph = new Graph(nodes, graphMap);
+    }
 
+    record Destination(String id, int cost) implements Comparable<Destination> {
+        @Override
+        public int compareTo(Destination o) {
+            return id.compareTo(o.id);
+        }
     }
 
     /*
@@ -155,6 +304,54 @@ public class Problem16 extends Problem {
      */
     @Override
     public Object rule1() {
+        var routeFinder = new RouteFinder(graph, new Scorer(), new Scorer());
+        var list = graph.nodes().values().stream().filter(node -> node.rate > 0 || node.id.equals("AA")).toList();
+        var map = new TreeMap<String, TreeSet<Destination>>();
+        var mapBis = new TreeMap<String, TreeSet<String>>();
+        var newNodes = new TreeMap<String, Node>();
+        for (var i = 0; i < list.size(); ++i) {
+            map.put(list.get(i).id, new TreeSet<>());
+            for (var j = 0; j < list.size(); ++j) {
+                if (i == j) continue;
+                var from = list.get(i);
+                var to = list.get(j);
+                if (to.id.equals("AA")) continue;
+                var route = routeFinder.findRoute(from, to);
+
+                var doublon = route.stream()
+                        .filter(node -> !node.equals(from))
+                        .filter(node -> !node.equals(to))
+                        .filter(node -> !node.equals(graph.getNode("AA")))
+                        .anyMatch(list::contains);
+                if (doublon) continue;
+                map.get(from.id).add(new Destination(to.id, route.size() - 1));
+            }
+        }
+        Stack<String> toHandleStack = new Stack<>();
+        toHandleStack.push("AA_1");
+        do {
+            var toHandle = toHandleStack.pop();
+            if (mapBis.containsKey(toHandle)) continue;
+            mapBis.put(toHandle, new TreeSet<>());
+            var id = toHandle.split("_")[0];
+            var number = Integer.parseInt(toHandle.split("_")[1]);
+            var rate = graph.getNode(id).rate;
+            var newNode = new Node(toHandle, rate, (30 - number) * rate);
+            newNodes.put(toHandle, newNode);
+            map.get(id).forEach(dest -> {
+                var destId = dest.id;
+                var newNumber = number + dest.cost + 1;
+                if (newNumber <= 30) {
+                    var newDest = destId + "_" + newNumber;
+                    mapBis.get(toHandle).add(newDest);
+                    toHandleStack.push(newDest);
+                }
+            });
+            if (mapBis.get(toHandle).isEmpty()) {
+                mapBis.get(toHandle).add("END_30");
+            }
+        } while (toHandleStack.size() > 0);
+
         return null;
     }
 
